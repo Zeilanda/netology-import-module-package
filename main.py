9bc798fbf21c3025015f96ec08fa7d9b8e186549
Zeilanda <dar.remez@gmail.com>
2022-06-19 12:04:39 +0300

    fix main.py

--- main.py
+++ main.py
@@ -4,4 +4,8 @@ from application.db.people import get_employees
 from application.salary import calculate_salary
 
 
-ccc
+if __name__ == '__main__':
+    calculate_salary()
+    get_employees()
+    current_date = date.today()
+    print(current_date)
